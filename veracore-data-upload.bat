643be8acda6f07bedf5124eb7a6cb5c82a3f04dd
ljannatipour <jannatipour.leena@3plwinner.com>
2025-08-14 14:29:40 -0700

    New .bat

--- veracore-data-upload.bat
+++ veracore-data-upload.bat
@@ -1,22 +1,8 @@
 @echo off
-cd /d "C:Users\Administrator\Scripts\InventoryHealthDashboard_DATA
+cd /d "C:\Users\Administrator\Scripts\InventoryHealthDashboard_DATA"
 
-IF NOT EXIST "venv" (
-    echo Creating virtual environment...
-    python -m venv venv
-    if errorlevel 1 (
-        echo Failed to create virtual environment.
-        exit /b 1
-    )
-) ELSE (
-    echo Virtual environment already exists.
-)
-
+:: Activate the existing virtual environment
 call venv\Scripts\activate.bat
 
-pip install -r requirements.txt --disable-pip-version-check --quiet
-
+:: Run the Python script
 python reports.py
-
-:: Only pause if running interactively (not in Task Scheduler)
-echo %CMDCMDLINE% | find /i "cmd.exe" >nul && pause
