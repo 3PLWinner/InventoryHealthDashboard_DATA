87d79aab47d13de45c2babc1ee33b87ae5d341ac
ljannatipour <jannatipour.leena@3plwinner.com>
2025-08-15 10:09:49 -0700

    Env in gitignore

--- veracore-data-upload.bat
+++ veracore-data-upload.bat
@@ -2,4 +2,6 @@
 cd /d "C:\Users\Administrator\Scripts\InventoryHealthDashboard_DATA"
 
 :: Activate virtual environment
-venv\Scripts\activate.bat reports.py
+::venv\Scripts\activate.bat 
+
+python reports.py
