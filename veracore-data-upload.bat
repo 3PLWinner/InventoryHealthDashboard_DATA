9694c2a0207862e2daea9c1b1ad55a8f94c85f42
ljannatipour <jannatipour.leena@3plwinner.com>
2025-08-18 09:11:46 -0700

    New requirements.txt

--- veracore-data-upload.bat
+++ veracore-data-upload.bat
@@ -3,7 +3,9 @@ echo Starting Veracore Data Retrieval at %date% %time%
 cd /d "C:\Users\Administrator\Scripts\InventoryHealthDashboard_DATA"
 
 :: Activate virtual environment
-call venv\Scripts\activate.bat 
+call .\venv\Scripts\activate.bat 
+
+pip install -r requirements.txt
 
 :: Run the script with output logging
 python reports.py >> script_log.txt 2>&1
