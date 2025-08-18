674ecadb43782b5fa1e4684b774a0221dccea9ed
ljannatipour <jannatipour.leena@3plwinner.com>
2025-08-18 08:43:30 -0700

    Debugging added to .bat

--- veracore-data-upload.bat
+++ veracore-data-upload.bat
@@ -1,7 +1,11 @@
 @echo off
+echo Starting Veracore Data Retrieval at %date% %time%
 cd /d "C:\Users\Administrator\Scripts\InventoryHealthDashboard_DATA"
 
 :: Activate virtual environment
 call venv\Scripts\activate.bat 
 
-python reports.py
+:: Run the script with output logging
+python reports.py >> script_log.txt 2>&1
+
+echo Script completed at %date% %time%
